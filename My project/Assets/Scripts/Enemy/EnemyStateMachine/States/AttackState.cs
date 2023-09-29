35fba07bdd141803c8c992de3e624bfbeaf1d427
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-09-29 17:53:43 +0300

    added attack animation

--- My project/Assets/Scripts/Enemy/EnemyStateMachine/States/AttackState.cs
+++ My project/Assets/Scripts/Enemy/EnemyStateMachine/States/AttackState.cs
@@ -9,6 +9,9 @@ public class AttackState : IStateSwitcher
     private Transform _npc;
     private PlayerHealth _target;
     private int _damage = 25;
+    private Quaternion _initialRotation;
+    private float _attackRate = 2.9f;
+    private float _time = 0f;
 
     public AttackState(StateMachine stateMachine, Animator animator, PlayerHealth target, Transform player, Transform npc)
     {
@@ -17,11 +20,13 @@ public class AttackState : IStateSwitcher
         _target = target;
         _player = player;
         _npc = npc;
+        _initialRotation = _npc.transform.rotation;
     }
 
     public void Enter()
     {
         _animator.SetBool(Constants.IsAttacking, true);
+        _time = 2.5f;
     }
 
     public void Exit()
@@ -36,15 +41,24 @@ public class AttackState : IStateSwitcher
 
     private void Attack()
     {
-        if(Vector3.Distance(_player.position, _npc.position) < _maxDistance)
+        _time += Time.deltaTime;
+
+        if (_time > _attackRate)
         {
-            _target.TakeDamage(_damage);
-            _npc.LookAt(_player.position);
-        }
-        else
-        {
-            _animator.SetBool(Constants.IsAttacking, false);
-            _stateMachine.SetRunningState();
+
+            if (Vector3.Distance(_player.position, _npc.position) < _maxDistance)
+            {
+                _target.TakeDamage(_damage);
+                _npc.LookAt(_player.position);
+                _npc.rotation = Quaternion.Euler(_initialRotation.eulerAngles.x, _npc.rotation.eulerAngles.y, _initialRotation.eulerAngles.z);
+            }
+            else
+            {
+                _animator.SetBool(Constants.IsAttacking, false);
+                _stateMachine.SetRunningState();
+            }
+
+            _time = 0;
         }
     }
 }
